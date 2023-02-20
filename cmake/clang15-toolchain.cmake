b76c14eeec7c6be918d9344b9e8d2c1fdc71fc30
Roman Granacher <roman_granacher@web.de>
2023-02-20 22:48:19 +0100

    style: Add clang-format + clang-tidy files, small fixes

--- cmake/clang15-toolchain.cmake
+++ cmake/clang15-toolchain.cmake
@@ -3,3 +3,5 @@ include_guard(GLOBAL)
 # Cache is needed so that vscode cmake-tools extension knows which debugger to use
 set(CMAKE_CXX_COMPILER clang++-15 CACHE STRING "CXX Compiler" FORCE)
 set(CMAKE_C_COMPILER clang-15 CACHE STRING "C Compiler" FORCE)
+
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
