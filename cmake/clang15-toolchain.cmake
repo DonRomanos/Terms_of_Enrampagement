7be4b5bbc3705fff6637984271ec3ba8fa3f94c6
Roman Granacher <roman_granacher@web.de>
2023-02-20 21:30:24 +0100

    build: Fix debugging for vscode cmake tools extension

--- cmake/clang15-toolchain.cmake
+++ cmake/clang15-toolchain.cmake
@@ -1,4 +1,5 @@
 include_guard(GLOBAL)
 
-set(CMAKE_CXX_COMPILER clang++-15)
-set(CMAKE_C_COMPILER clang-15)
+# Cache is needed so that vscode cmake-tools extension knows which debugger to use
+set(CMAKE_CXX_COMPILER clang++-15 CACHE STRING "CXX Compiler" FORCE)
+set(CMAKE_C_COMPILER clang-15 CACHE STRING "C Compiler" FORCE)
