5d25adcd167a940112668fc10d95291b69f31c15
Roman Granacher <roman_granacher@web.de>
2023-03-06 20:56:03 +0100

    math: Add math library

---
+++ src/math/include/math/vector.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace math
+{
+template <typename T>
+struct Vector2D {
+    T x;
+    T y;
+};
+}  // namespace math
---
+++ src/math/test/vector_tests.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+#include <math/vector.hpp>
+
+TEST(Vector, Supports_Addition) { FAIL(); }
