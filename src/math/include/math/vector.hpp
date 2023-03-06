32c0fd082bf4df9e43b4025afcf36768428851a8
Roman Granacher <roman_granacher@web.de>
2023-03-06 22:38:53 +0100

    math: Basic vector class with arithmetic operations

--- src/math/include/math/vector.hpp
+++ src/math/include/math/vector.hpp
@@ -6,5 +6,48 @@ template <typename T>
 struct Vector2D {
     T x;
     T y;
+
+    constexpr Vector2D& operator+=(const Vector2D& other)
+    {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+
+    constexpr Vector2D& operator-=(const Vector2D& other)
+    {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+
+    friend constexpr Vector2D operator+(const Vector2D& lhs,
+                                        const Vector2D& rhs)
+    {
+        return Vector2D(lhs) += rhs;
+    }
+
+    friend constexpr Vector2D operator-(const Vector2D& lhs,
+                                        const Vector2D& rhs)
+    {
+        return Vector2D(lhs) -= rhs;
+    }
+
+    constexpr Vector2D& operator*=(const T factor)
+    {
+        x *= factor;
+        y *= factor;
+        return *this;
+    }
+
+    constexpr Vector2D operator*(const T factor)
+    {
+        return Vector2D(*this) *= factor;
+    }
+
+    friend constexpr bool operator<=>(const Vector2D&,
+                                      const Vector2D&) = default;
 };
+
+using Vector2i = Vector2D<int>;
 }  // namespace math
--- src/math/test/vector_tests.cpp
+++ src/math/test/vector_tests.cpp
@@ -2,4 +2,26 @@
 
 #include <math/vector.hpp>
 
-TEST(Vector, Supports_Addition) { FAIL(); }
+namespace math
+{
+TEST(Vector, Supports_arithmetic_operations)
+{
+    // Addition
+    static_assert((Vector2i{.x = 1, .y = 2} + Vector2i{.x = 2, .y = 1}) ==
+                  Vector2i{.x = 3, .y = 3});
+    static_assert((Vector2i{.x = 1, .y = 2} += Vector2i{.x = 1, .y = 2}) ==
+                  Vector2i{.x = 2, .y = 4});
+
+    // Subtraction
+    static_assert((Vector2i{.x = 1, .y = 2} -= Vector2i{.x = 2, .y = 1}) ==
+                  Vector2i{.x = -1, .y = 1});
+    static_assert((Vector2i{.x = 5, .y = 4} - Vector2i{.x = 1, .y = 2}) ==
+                  Vector2i{.x = 4, .y = 2});
+
+    // Multiplication
+    static_assert((Vector2i{.x = 1, .y = 2} *= 3) == Vector2i{.x = 3, .y = 6});
+    static_assert((Vector2i{.x = 2, .y = -1} * 3) == Vector2i{.x = 6, .y = -3});
+
+    SUCCEED();
+}
+}  // namespace math
